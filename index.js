f9010951938115a053b6f0001787c839205edfd9
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-20 13:48:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your code here
+// // Write your code here
+// class Dog{
+//     constructor(name, breed){
+//         this.name = name
+//         this.breed = breed
+//     }
+// }
+
+class Breakfast {
+    constructor(food, drink){
+        this.food = food
+        this.drink = drink
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad
+        this.soup = soup
+        this.drink = drink
+    }
+}
+class Dinner{
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad
+        this.soup = soup
+        this.entree = entree
+        this._dessert = dessert
+    }
+}
